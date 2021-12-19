461f3c46a5324dfa1ea6ad7b2c28321ae51e9fb6
sntulix <shizu@futuregadget.com>
2021-12-19 15:20:49 +0900

    add mat_blit(), mat_rect_blit(), setMosaic() to kanja.js.

--- kanja/kanja.js
+++ kanja/kanja.js
@@ -177,6 +177,7 @@ if (0<url_params.length) {
 
 function addMosaic_to_canvas_and_build_upload_image() {
   let src = cv.imread("canvasInput");
+  let srcBackup = src.clone();
   let gray = new cv.Mat();
   cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
   // detect faces
@@ -190,21 +191,72 @@ function addMosaic_to_canvas_and_build_upload_image() {
       let point1 = new cv.Point(faces.get(i).x, faces.get(i).y);
       let point2 = new cv.Point(faces.get(i).x + faces.get(i).width,
                                 faces.get(i).y + faces.get(i).height);
+      /* mosaic to face */
+      setMosaic(src, faces.get(i).x, faces.get(i).y, faces.get(i).width, faces.get(i).height);
       cv.rectangle(src, point1, point2, [255, 0, 0, 255]);
+
       // detect eyes in face ROI
       eyeCascade.detectMultiScale(roiGray, eyes);
       for (let j = 0; j < eyes.size(); ++j) {
           let point1 = new cv.Point(eyes.get(j).x, eyes.get(j).y);
           let point2 = new cv.Point(eyes.get(j).x + eyes.get(j).width,
                                     eyes.get(j).y + eyes.get(j).height);
+          mat_rect_blit(srcBackup, faces.get(i).x+eyes.get(j).x, faces.get(i).y+eyes.get(j).y, eyes.get(j).width, eyes.get(j).height,
+            src, faces.get(i).x+eyes.get(j).x, faces.get(i).y+eyes.get(j).y);
           cv.rectangle(roiSrc, point1, point2, [0, 0, 255, 255]);
       }
       roiGray.delete(); roiSrc.delete();
   }
+
   cv.imshow("canvasInput", src);
+  src.delete(); gray.delete(); srcBackup.delete(); faces.delete(); eyes.delete();
+
   let jpeg_base64 = canvasInput.toDataURL('image/jpeg', 0.6).substr(23);
   document.querySelector(".inputtag_photo_base64").value = jpeg_base64;
-  src.delete(); gray.delete(); faces.delete(); eyes.delete();
+}
+
+function setMosaic(src, x, y, width, height) {
+  let mosaic_mat = new cv.Mat();
+  let copy_rect = new cv.Rect(x, y, width, height);
+  mosaic_mat = src.roi(copy_rect);
+
+  let small_size = new cv.Size(10, 10);
+  let original_size = new cv.Size(width, height);
+  cv.resize(mosaic_mat, mosaic_mat, small_size, 1, 1, cv.INTER_NEAREST);
+  cv.resize(mosaic_mat, mosaic_mat, original_size, 1, 1, cv.INTER_NEAREST);
+
+  mat_blit(mosaic_mat, src, x, y)
+}
+
+function mat_blit(src_mat, dest_mat, dest_mat_start_x, dest_mat_start_y) {
+  let dest_mat_start_pixel = (dest_mat_start_y*dest_mat.cols*dest_mat.channels())+(dest_mat_start_x*dest_mat.channels());
+  let src_mat_pixel_pos = 0;
+  for (var y = 0; y<src_mat.size().height; y++) {
+    for (var x = 0; x<src_mat.size().width; x++) {
+      let dest_mat_current_pos = (x*dest_mat.channels())+(y*dest_mat.cols*dest_mat.channels()) + dest_mat_start_pixel;
+      for (let c = 0; c<dest_mat.channels(); c++) {
+        dest_mat.data[dest_mat_current_pos+c] = src_mat.data[src_mat_pixel_pos+c];
+      }
+      src_mat_pixel_pos = src_mat_pixel_pos + dest_mat.channels();
+    }
+  }
+}
+
+function mat_rect_blit(src_mat, src_mat_start_x, src_mat_start_y, src_rect_width, src_rect_height,
+  dest_mat, dest_mat_start_x, dest_mat_start_y) {
+  for (var y = 0; y<src_rect_height; y++) {
+    for (var x = 0; x<src_rect_width; x++) {
+      let src_mat_current_pos = ((src_mat.channels()*src_mat.cols)*(src_mat_start_y+y))
+       + (src_mat.channels()*(src_mat_start_x+x));
+
+      let dest_mat_current_pos = ((dest_mat.channels()*dest_mat.cols)*(dest_mat_start_y+y))
+       + (dest_mat.channels()*(dest_mat_start_x+x));
+
+      for (let c = 0; c<dest_mat.channels(); c++) {
+        dest_mat.data[dest_mat_current_pos+c] = src_mat.data[src_mat_current_pos+c];
+      }
+    }
+  }
 }
 
 let utils = new Utils('errorOutput');
